700e256f1f2bb28fc0eb0d93fba11e76bfe81a2d
theninx <the.well@hotmail.com>
2022-08-15 17:05:20 -0300

    API NodeJs mysql

---
+++ api/index.js
@@ -0,0 +1,24 @@
+const express = require('express')
+const bodyParse = require('body-parser')
+
+const app = express()
+
+app.use(bodyParse.json())
+
+const port = 3000
+
+app.get('/teste', (req,res) =>
+
+    res.status(200).send({msg: 'API 100%'})
+
+)
+
+app.listen(port,() =>
+
+    console.log("Servidor rodando na porta 3000")
+
+)
+
+module.exports = app
+
+
