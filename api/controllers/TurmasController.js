81774fd11955aaa929059850b2b4085e4982b759
w-ingles <wingles@girolando.com.br>
2022-08-30 17:20:11 -0300

    Criando crud de turmas

--- api/controllers/TurmasController.js
+++ api/controllers/TurmasController.js
@@ -12,6 +12,74 @@ class TurmasController{
         }
     }
 
+    static async pegarTurma(req, res) {
+
+        const {id} = req.params
+
+        try {
+            const Turma = await database.Turmas.findOne({
+                where: {
+                    id: Number(id)
+                }
+            })
+
+            if (Turma === null) {
+                return res.status(500).send("Não foi possivel encontrar nem uma informação")
+            }
+
+            return res.status(200).json(Turma)
+
+        } catch (e) {
+
+            return res.status(500).send(`Não foi possivel executar sua consulta possivel motivo:  ${e.message}`)
+        }
+
+
+    }
+
+    static async criaTurma(req, res) {
+        const novoNivel = req.body
+
+        try {
+            const TurmaCriado = await database.Turmas.create(novoNivel)
+            return res.status(200).json(TurmaCriado)
+
+        } catch (e) {
+            return res.status(500).send(e.message)
+        }
+
+    }
+
+
+    static async atualizaTurma(req, res) {
+
+        const {id} = req.params
+        const novaInformacao = req.body
+        try {
+            await database.Turmas.update(novaInformacao, {where: {id: Number(id)}})
+
+            const turmaAtualizada = await database.Turmas.findOne({where: {id: Number(id)}})
+
+            return res.status(200).json(turmaAtualizada)
+
+        } catch (e) {
+            return res.status(500).send(e.message)
+        }
+    }
+
+    static async deleteTurma(req,res){
+        const {id} = req.params
+        try {
+            await database.Turmas.destroy({where:{id: Number(id)}})
+
+            return res.status(200).json({removido: true})
+
+        }catch (e) {
+            return res.status(500).send(e.message)
+        }
+
+    }
+
 }
 
 module.exports = TurmasController
--- api/routes/turmasRoute.js
+++ api/routes/turmasRoute.js
@@ -1,10 +1,15 @@
 const {Router} = require('express')
 const TurmasController = require('../controllers/TurmasController')
 
+
 const router = Router()
 
 
-router.get      ('/Turmas',        TurmasController.pegaTodasAsTurmas)
+router.get      ('/Turmas',              TurmasController.pegaTodasAsTurmas)
+router.get      ('/Turmas/:id',          TurmasController.pegarTurma)
+router.post     ('/Turmas',              TurmasController.criaTurma)
+router.put      ('/Turmas/:id',          TurmasController.atualizaTurma)
+router.delete   ('/Turmas/:id',          TurmasController.deleteTurma)
 
 
 module.exports = router
