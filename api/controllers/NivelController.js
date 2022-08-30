f85d60027cfc92ad9507336e4e279917494d4c05
w-ingles <wingles@girolando.com.br>
2022-08-30 17:09:56 -0300

    Finalizando crud niveis

--- api/controllers/NivelController.js
+++ api/controllers/NivelController.js
@@ -12,7 +12,7 @@ class NivelController {
         }
     }
 
-    static async pegarNivel(req,res) {
+    static async pegarNivel(req, res) {
 
         const {id} = req.params
 
@@ -23,13 +23,13 @@ class NivelController {
                 }
             })
 
-            if (Nivel === null){
+            if (Nivel === null) {
                 return res.status(500).send("Não foi possivel encontrar nem uma informação")
             }
 
             return res.status(200).json(Nivel)
 
-        }catch (e){
+        } catch (e) {
 
             return res.status(500).send(`Não foi possivel executar sua consulta possivel motivo:  ${e.message}`)
         }
@@ -37,19 +37,61 @@ class NivelController {
 
     }
 
-    static async criaNivel(req,res) {
+    static async criaNivel(req, res) {
         const novoNivel = req.body
 
         try {
             const NivelCriado = await database.Niveis.create(novoNivel)
             return res.status(200).json(NivelCriado)
 
-        }catch (e){
+        } catch (e) {
             return res.status(500).send(e.message)
         }
 
     }
 
+    static async procurandoNivelPorNome(req, res) {
+        const parametroDeBusca = req.body.parametro
+        console.log(req.body.parametro)
+        try {
+            const Nivel = await database.Niveis.findOne({where: {descr_nivel: parametroDeBusca}})
+            return res.status(200).json(Nivel)
+
+        } catch (e) {
+            return res.status(500).send(e.message)
+        }
+
+    }
+
+
+    static async atualizaNivel(req, res) {
+
+        const {id} = req.params
+        const novaInformacao = req.body
+        try {
+            await database.Niveis.update(novaInformacao, {where: {id: Number(id)}})
+
+            const niveistualizada = await database.Niveis.findOne({where: {id: Number(id)}})
+
+            return res.status(200).json(niveistualizada)
+
+        } catch (e) {
+            return res.status(500).send(e.message)
+        }
+    }
+
+    static async deleteNivel(req,res){
+        const {id} = req.params
+        try {
+            await database.Niveis.destroy({where:{id: Number(id)}})
+
+            return res.status(200).json({removido: true})
+
+        }catch (e) {
+            return res.status(500).send(e.message)
+        }
+
+    }
 
 
 }
--- api/routes/niveisRoute.js
+++ api/routes/niveisRoute.js
@@ -7,9 +7,14 @@ const NivelController = require('../controllers/NivelController')
 const router = Router()
 
 
-router.get      ('/Niveis',            NivelController.pegaTodosOsNiveis)
-router.get      ('/Niveis/:id',        NivelController.pegarNivel)
-router.post      ('/Niveis',           NivelController.criaNivel)
+router.get      ('/Niveis',              NivelController.pegaTodosOsNiveis)
+router.get      ('/Niveis/:nome',        NivelController.procurandoNivelPorNome)
+router.get      ('/Niveis/:id',          NivelController.pegarNivel)
+router.post     ('/Niveis',              NivelController.criaNivel)
+router.put      ('/Niveis/:id',          NivelController.atualizaNivel)
+router.delete   ('/Niveis/:id',          NivelController.deleteNivel)
+
+
 
 
 module.exports = router
