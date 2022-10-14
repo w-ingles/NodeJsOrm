3e8a85968ab84f01e57185ec0c86ec8652a8b0ec
w-ingles <wingles@girolando.com.br>
2022-10-14 08:42:41 -0300

    Criando Services de Niveis/Turmas

---
+++ api/services/NiveisServices.js
@@ -0,0 +1,13 @@
+const Services = require('../services/Services')
+
+
+class NiveisServices extends Services {
+
+
+    constructor() {
+        super('Niveis');
+    }
+
+}
+
+module.exports = NiveisServices
---
+++ api/services/TurmasServices.js
@@ -0,0 +1,13 @@
+const Services = require('../services/Services')
+
+
+class TurmasServices extends Services {
+
+
+    constructor() {
+        super('Turmas');
+    }
+
+}
+
+module.exports = TurmasServices
