9c92898a1db1ea316739011110b4699fe35fe0a7
w-ingles <wingles@girolando.com.br>
2022-10-14 08:37:12 -0300

    Criando Services de pessoas

---
+++ api/services/PessoasServices.js
@@ -0,0 +1,13 @@
+const Services = require('../services/Services')
+
+
+class PessoasServices extends Services {
+
+
+    constructor() {
+        super('Pessoas');
+    }
+
+}
+
+module.exports = PessoasServices
